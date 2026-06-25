da9ef17b75b7b9d21ac2bd4bdb8c9d9532108ca0
Swatisagarika <swatisagarika001@gmail.com>
2026-06-25 21:50:03 +0530

    Initial commit

---
+++ netlify/functions/send-mail.js
@@ -0,0 +1,46 @@
+const nodemailer = require("nodemailer");
+
+exports.handler = async (event) => {
+  try {
+    const { name, email, phone, message } = JSON.parse(event.body);
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.SMTP_EMAIL,
+      to: process.env.SMTP_EMAIL,
+      subject: `Portfolio Contact from ${name}`,
+      html: `
+        <h2>New Contact Form</h2>
+
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Phone:</b> ${phone}</p>
+        <p><b>Message:</b></p>
+
+        <p>${message}</p>
+      `,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+      }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        success: false,
+        error: err.message,
+      }),
+    };
+  }
+};
--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -44,7 +44,7 @@ function Contact({hideNavbar}) {
   setLoading(true);
 
   try {
-    const response = await fetch("http://localhost:5000/api/contact", {
+    const response = await fetch("/.netlify/functions/send-mail",{
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData)
